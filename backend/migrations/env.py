4dd12d40afedd4fb6e27f9ae9d7de757e71345d0
backspacevenkat <ghanta.v.subrhmanyam@gmail.com>
2025-02-18 22:50:02 -0800

    Auto commit 2025-02-18 22:50:02

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -36,7 +36,7 @@ def run_migrations_offline() -> None:
     Calls to context.execute() here emit the given string to the
     script output.
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = "postgresql://neondb_owner:npg_4GK5QbBnqzdk@ep-lively-darkness-a6zoh3mw-pooler.us-west-2.aws.neon.tech/neondb?sslmode=require"
     context.configure(
         url=url,
         target_metadata=target_metadata,
