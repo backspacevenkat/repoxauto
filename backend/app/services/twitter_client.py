e441494281b2dcb46ba8b24bb15d3b865cd2afb5
backspacevenkat <ghanta.v.subrhmanyam@gmail.com>
2025-02-19 20:04:43 -0800

    Auto commit 2025-02-19 20:04:43

--- backend/app/services/twitter_client.py
+++ backend/app/services/twitter_client.py
@@ -1509,7 +1509,7 @@ class TwitterClient:
             "premium_content_api_read_enabled": False,
             "communities_web_enable_tweet_community_results_fetch": True,
             "c9s_tweet_anatomy_moderator_badge_enabled": True,
-                "withReplies": True,
+            "responsive_web_grok_analyze_button_fetch_trends_enabled": False,
                 "withRuxInjections": False,
                 "withClientEventToken": False,
                 "withBirdwatchPivots": False,
