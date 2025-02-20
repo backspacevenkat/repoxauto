5a3ecb7916ba053241d5aa8b367a36a5408e30a5
backspacevenkat <ghanta.v.subrhmanyam@gmail.com>
2025-02-19 17:24:24 -0800

    Auto commit 2025-02-19 17:24:24

--- backend/app/services/task_processor.py
+++ backend/app/services/task_processor.py
@@ -450,3 +450,4 @@ class TaskProcessor:
                 profile_banner=meta_data.get("profile_banner"),
                 lang=meta_data.get("lang"),
                 new_login=meta_data.get("new_login")
+            )
