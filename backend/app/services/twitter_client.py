498a8742fb6452140d5504763a1168a2cf6bc663
backspacevenkat <ghanta.v.subrhmanyam@gmail.com>
2025-02-19 20:05:08 -0800

    Auto commit 2025-02-19 20:05:08

--- backend/app/services/twitter_client.py
+++ backend/app/services/twitter_client.py
@@ -1532,7 +1532,7 @@ class TwitterClient:
             "c9s_tweet_anatomy_moderator_badge_enabled": True,
             "responsive_web_grok_analyze_button_fetch_trends_enabled": False,
             "responsive_web_grok_analyze_post_followups_enabled": False,
-            timeline_paths = [
+            "responsive_web_grok_share_attachment_enabled": True,
                 lambda: response.get('data', {}).get('user', {}).get('result', {}).get('timeline_v2', {}).get('timeline', {}),
                 lambda: response.get('data', {}).get('user', {}).get('result', {}).get('timeline', {}),
                 lambda: response.get('data', {}).get('user', {}).get('result', {}).get('tweets_timeline', {}).get('timeline', {}),
